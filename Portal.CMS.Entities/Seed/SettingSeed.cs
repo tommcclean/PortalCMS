44c16d903eca672f78e5dfdedd00e6100f068e72
Tom McClean <tommcclean@hotmail.co.uk>
2016-07-11 21:13:44 +0100

    Publish Description Meta Tag Setting

--- Portal.CMS.Entities/Seed/SettingSeed.cs
+++ Portal.CMS.Entities/Seed/SettingSeed.cs
@@ -14,7 +14,7 @@ namespace Portal.CMS.Entities.Seed
 
             if (!context.Settings.Any(x => x.SettingName == "Description Meta Tag"))
             {
-                context.Settings.Add(new Setting() { SettingName = "Website Name", SettingValue = "Portal CMS is a fully featured content management system with a powerful integrated page builder." });
+                context.Settings.Add(new Setting() { SettingName = "Description Meta Tag", SettingValue = "Portal CMS is a fully featured content management system with a powerful integrated page builder." });
             }
         }
     }
