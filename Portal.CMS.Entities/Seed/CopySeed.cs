43fb5ae43f1f1b7cf471f251a0eb0678a392dc62
Tom McClean <tommcclean@hotmail.co.uk>
2016-07-11 19:49:52 +0100

    Seed Copy Sections for Analytics and Meta Description

---
+++ Portal.CMS.Entities/Seed/CopySeed.cs
@@ -0,0 +1,19 @@
+﻿using Portal.CMS.Entities.Entities.PageBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Portal.CMS.Entities.Seed
+{
+    public class CopySeed
+    {
+        public static void Seed(PortalEntityModel context)
+        {
+            if (!context.CopySections.Any())
+            {
+                context.CopySections.Add(new Entities.Copy.Copy() { CopyName = "Description Meta-Tag", CopyBody = "<meta name=\"description\" content=\"EyeDentity gives you easy to use tools to quickly create your own web pages for free, no coding skills required.\">", DateAdded = DateTime.Now, DateUpdated = DateTime.Now });
+                context.CopySections.Add(new Entities.Copy.Copy() { CopyName = "Analytics", CopyBody = "", DateAdded = DateTime.Now, DateUpdated = DateTime.Now });
+            }
+        }
+    }
+}
