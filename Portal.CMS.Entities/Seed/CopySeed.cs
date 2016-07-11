86b9a2001b1a2c25d91fef34cbfdace827998c77
Tom McClean <tommcclean@hotmail.co.uk>
2016-07-11 19:52:45 +0100

    Update Copy Seed Scripts

--- Portal.CMS.Entities/Seed/CopySeed.cs
+++ Portal.CMS.Entities/Seed/CopySeed.cs
@@ -11,7 +11,7 @@ namespace Portal.CMS.Entities.Seed
         {
             if (!context.CopySections.Any())
             {
-                context.CopySections.Add(new Entities.Copy.Copy() { CopyName = "Description Meta-Tag", CopyBody = "<meta name=\"description\" content=\"EyeDentity gives you easy to use tools to quickly create your own web pages for free, no coding skills required.\">", DateAdded = DateTime.Now, DateUpdated = DateTime.Now });
+                context.CopySections.Add(new Entities.Copy.Copy() { CopyName = "Description Meta-Tag", CopyBody = "<meta name=\"description\" content=\"Portal CMS is a fully featured content management system with a powerful integrated page builder.\">", DateAdded = DateTime.Now, DateUpdated = DateTime.Now });
                 context.CopySections.Add(new Entities.Copy.Copy() { CopyName = "Analytics", CopyBody = "", DateAdded = DateTime.Now, DateUpdated = DateTime.Now });
             }
         }
