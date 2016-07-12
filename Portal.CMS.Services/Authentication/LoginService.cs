20209c0723d9f4a69c5ecdbc2d3293972ef61f84
Tom McClean <tommcclean@hotmail.co.uk>
2016-07-12 23:40:42 +0100

    Remove SecurityHelper Static Helper

--- Portal.CMS.Services/Authentication/LoginService.cs
+++ Portal.CMS.Services/Authentication/LoginService.cs
@@ -1,6 +1,8 @@
 ﻿using Portal.CMS.Entities;
 using Portal.CMS.Services.Shared;
+using System;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace Portal.CMS.Services.Authentication
 {
@@ -24,10 +26,29 @@ namespace Portal.CMS.Services.Authentication
             if (userAccount == null)
                 return null;
 
-            if (!SecurityHelper.CompareSecurePassword(password, userAccount.Password))
+            if (!CompareSecurePassword(password, userAccount.Password))
                 return null;
 
             return userAccount.UserId;
+        }
+
+        private bool CompareSecurePassword(string passwordAttempt, string passwordActual)
+        {
+            string savedPasswordHash = passwordActual;
+            /* Extract the bytes */
+            byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+            /* Get the salt */
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            /* Compute the hash on the password the user entered */
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordAttempt, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            /* Compare the results */
+            for (int i = 0; i < 20; i++)
+                if (hashBytes[i + 16] != hash[i])
+                    return false;
+
+            return true;
         }
     }
 }
--- Portal.CMS.Services/Authentication/RegistrationService.cs
+++ Portal.CMS.Services/Authentication/RegistrationService.cs
@@ -3,6 +3,7 @@ using Portal.CMS.Entities.Entities.Authentication;
 using Portal.CMS.Services.Shared;
 using System;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace Portal.CMS.Services.Authentication
 {
@@ -27,7 +28,7 @@ namespace Portal.CMS.Services.Authentication
             var userAccount = new User()
             {
                 EmailAddress = emailAddress,
-                Password = SecurityHelper.GenerateSecurePassword(password),
+                Password = GenerateSecurePassword(password),
                 GivenName = givenName,
                 FamilyName = familyName,
                 DateAdded = DateTime.Now,
@@ -39,6 +40,25 @@ namespace Portal.CMS.Services.Authentication
             _context.SaveChanges();
 
             return userAccount.UserId;
+        }
+
+        private string GenerateSecurePassword(string password)
+        {
+            // http://stackoverflow.com/questions/4181198/how-to-hash-a-password
+
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+
+            return savedPasswordHash;
         }
     }
 }
