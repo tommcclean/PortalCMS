c0bd1addddef590890297bba244648e606c3d2c2
Tom McClean <tommcclean@hotmail.co.uk>
2016-09-01 19:42:36 +0100

    Add FAQ / Spoiler Component Animations

--- Portal.CMS.Web/Areas/Builder/Content/Scripts/component.expand.js
+++ Portal.CMS.Web/Areas/Builder/Content/Scripts/component.expand.js
@@ -1,20 +1,23 @@
 ﻿$(document).ready(function () {
+    $(".visitor .component-expand .component-body").first().addClass("active").slideDown();
+
     $(".visitor .component-expand").click(function (event) {
         var componentBody = $(this).find('.component-body');
 
         if (componentBody.hasClass('active'))
         {
-            componentBody.slideUp();
+            componentBody.slideUp(300);
 
             componentBody.removeClass("active");
         }
         else
         {
-            componentBody.slideDown();
+            $('.visitor .component-expand .component-body.active').removeClass("active").slideUp();
+
+            componentBody.slideDown(300);
 
             componentBody.addClass("active");
         }
 
-        
     });
 });
