c15a13d44f0717b1f73a650b0351f724ec30456e
tommcclean <tom.mcclean@rivermarketing.co.uk>
2016-09-07 17:15:13 +0100

    Begin Using BundleConfig for Script and Style Resources

--- Portal.CMS.Web/App_Start/BundleConfig.cs
+++ Portal.CMS.Web/App_Start/BundleConfig.cs
@@ -4,9 +4,31 @@ namespace Portal.CMS.Web
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+
+            #region Script Bundles
+
+            //bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap/Confirmation")
+                .Include("~/Areas/Admin/Content/Plugins/Confirmation/bootstrap-confirmation.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap/Modal")
+                .Include("~/Areas/Admin/Content/Scripts/modal.min.js"));
+
+            #endregion Script Bundles
+
+            #region Style Bundles
+
+            //bundles.Add(new StyleBundle("~/Resources/CSS/Bootstrap").Include(
+            //    "~/Content/styles/plugins/bootstrap-notify/*.css", new CssRewriteUrlTransform()));
+
+            //bundles.Add(new StyleBundle("~/Resources/CSS/JQueryUI").Include(
+            //    "~/Content/styles/plugins/bootstrap-popover/*.css", new CssRewriteUrlTransform()));
+
+            #endregion Style Bundles
         }
     }
 }
