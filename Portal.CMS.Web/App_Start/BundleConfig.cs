d54098686c91a2bee133c18df10107f5fab75e8e
Tom McClean <tommcclean@hotmail.co.uk>
2016-09-07 20:47:24 +0100

    Overhaul How Resources and Plugins are Retrieved and Stored in Portal CMS - Utilise .NET Bundling

--- Portal.CMS.Web/App_Start/BundleConfig.cs
+++ Portal.CMS.Web/App_Start/BundleConfig.cs
@@ -10,23 +10,37 @@ namespace Portal.CMS.Web
 
             #region Script Bundles
 
-            //bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js").Include("~/Content/Scripts/Bootstrap/bootstrap.min.js"));
 
-            bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap/Confirmation")
-                .Include("~/Areas/Admin/Content/Plugins/Confirmation/bootstrap-confirmation.min.js"));
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap/Confirmation").Include("~/Content/Scripts/BootstrapConfirmation/bootstrap-confirmation.min.js"));
 
-            bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap/Modal")
-                .Include("~/Areas/Admin/Content/Scripts/modal.min.js"));
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap/Modal").Include("~/Content/Scripts/Framework/modal.js"));
+
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/FancyBox").Include("~/Content/Scripts/FancyBox/*.js"));
+
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/C3Graphing").Include("~/Content/Scripts/C3Graphing/c3.min.js").Include("~/Content/Scripts/C3Graphing/d3.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/JQueryTouch").Include("~/Content/Scripts/JQueryTouch/jquery.ui.touch-punch.min.js"));
+
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/TinyMCE").Include("~/Content/Scripts/TinyMCE/tinymce.min.js").Include("~/Content/Scripts/TinyMCE/initialise.js"));
+
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/ImageSelector").Include("~/Content/Scripts/ImageSelector/*.js"));
 
             #endregion Script Bundles
 
             #region Style Bundles
 
-            //bundles.Add(new StyleBundle("~/Resources/CSS/Bootstrap").Include(
-            //    "~/Content/styles/plugins/bootstrap-notify/*.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/Resources/CSS/Bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css").Include("~/Content/Styles/Bootstrap/bootstrap.min.css", new CssRewriteUrlTransform()));
 
-            //bundles.Add(new StyleBundle("~/Resources/CSS/JQueryUI").Include(
-            //    "~/Content/styles/plugins/bootstrap-popover/*.css", new CssRewriteUrlTransform()));
+            bundles.Add(new StyleBundle("~/Resources/CSS/FancyBox").Include("~/Content/Styles/FancyBox/*.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/Resources/CSS/C3Graphing").Include("~/Content/Styles/C3Graphing/*.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/Resources/CSS/Framework").Include("~/Content/Styles/Framework/*.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/Resources/CSS/PageBuilder/Framework").Include("~/Areas/Builder/Content/Styles/*.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/Resources/CSS/Administration/Framework").Include("~/Areas/Admin/Content/Styles/Framework/*.css", new CssRewriteUrlTransform()));
 
             #endregion Style Bundles
         }
