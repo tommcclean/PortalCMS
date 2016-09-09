eeaca2d6fada6542d1fb91e66830c5a646470ba2
Tom McClean <tommcclean@hotmail.co.uk>
2016-09-09 22:41:06 +0100

    Resolve Strange Git Issue Where Files Are Not Checking In

---
+++ Portal.CMS.Web/Areas/Builder/ViewModels/Shared/PagesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Portal.CMS.Entities.Entities.PageBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.CMS.Web.Areas.Builder.ViewModels.Build
+{
+    public class PagesViewModel
+    {
+        public List<Page> PageList { get; set; }
+    }
+}
