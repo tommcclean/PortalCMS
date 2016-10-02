bb384e30de1f2870530f3d8b5c49f055b105828f
Tom McClean <tommcclean@hotmail.co.uk>
2016-10-02 14:07:45 +0100

    Implement Bootstrap Tour for New Users

--- Portal.CMS.Web/App_Start/BundleConfig.cs
+++ Portal.CMS.Web/App_Start/BundleConfig.cs
@@ -14,6 +14,8 @@ namespace Portal.CMS.Web
 
             bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap/Popover").Include("~/Content/Scripts/Bootstrap/initialise.js"));
 
+            bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap/Tour").Include("~/Content/Scripts/BootstrapTour/bootstrap-tour.min.js"));
+
             bundles.Add(new ScriptBundle("~/Resources/JavaScript/Bootstrap/Confirmation").Include("~/Content/Scripts/BootstrapConfirmation/bootstrap-confirmation.min.js"));
 
             bundles.Add(new ScriptBundle("~/Resources/JavaScript/FontAwesome/Picker").Include("~/Content/Scripts/FontAwesomePicker/fontawesome-iconpicker.min.js"));
@@ -43,6 +45,8 @@ namespace Portal.CMS.Web
             #region Style Bundles
 
             bundles.Add(new StyleBundle("~/Resources/CSS/Bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css").Include("~/Content/Styles/Bootstrap/bootstrap.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/Resources/CSS/Bootstrap/Tour").Include("~/Content/Styles/BootstrapTour/bootstrap-tour.min.css"));
 
             bundles.Add(new StyleBundle("~/Resources/CSS/FontAwesome/Picker").Include("~/Content/Styles/FontAwesomePicker/fontawesome-iconpicker.min.css"));
 
