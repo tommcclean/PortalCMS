2977e211fe58bb9f710bdc557306fc9b02bd6290
Tom McClean <tommcclean@hotmail.co.uk>
2017-02-28 21:40:52 +0000

    CSS Refactoring - Reduce Footprint

--- Portal.CMS.Web/Areas/Admin/Controllers/SettingManagerController.cs
+++ Portal.CMS.Web/Areas/Admin/Controllers/SettingManagerController.cs
@@ -1,6 +1,4 @@
-﻿using Portal.CMS.Services.Authentication;
-using Portal.CMS.Services.Menu;
-using Portal.CMS.Services.Settings;
+﻿using Portal.CMS.Services.Settings;
 using Portal.CMS.Web.Architecture.ActionFilters;
 using Portal.CMS.Web.Architecture.Helpers;
 using Portal.CMS.Web.Areas.Admin.ViewModels.SettingManager;
@@ -14,85 +12,13 @@ namespace Portal.CMS.Web.Areas.Admin.Controllers
         #region Dependencies
 
         readonly ISettingService _settingService;
-        readonly IRoleService _roleService;
-        readonly IMenuService _menuService;
 
-        public SettingManagerController(ISettingService settingService, IRoleService roleService, IMenuService menuService)
+        public SettingManagerController(ISettingService settingService)
         {
             _settingService = settingService;
-            _roleService = roleService;
-            _menuService = menuService;
         }
 
         #endregion Dependencies
-
-        [HttpGet]
-        public ActionResult Index()
-        {
-            var model = new SettingsViewModel
-            {
-                SettingList = _settingService.Get(),
-                RoleList = _roleService.Get(),
-                MenuList = _menuService.Get()
-            };
-
-            return View(model);
-        }
-
-        [HttpGet]
-        public ActionResult Create()
-        {
-            return View("_Create", new CreateViewModel());
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(CreateViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return View("_Create", model);
-
-            _settingService.Add(model.SettingName, model.SettingValue);
-
-            return this.Content("Refresh");
-        }
-
-        [HttpGet]
-        public ActionResult Edit(int settingId)
-        {
-            var setting = _settingService.Get(settingId);
-
-            var model = new EditViewModel
-            {
-                SettingId = settingId,
-                SettingName = setting.SettingName,
-                SettingValue = setting.SettingValue
-            };
-
-            return View("_Edit", model);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(EditViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return View("_Edit", model);
-
-            _settingService.Edit(model.SettingId, model.SettingName, model.SettingValue);
-
-            Session.Remove($"Setting-{model.SettingName}");
-
-            return this.Content("Refresh");
-        }
-
-        [HttpGet]
-        public ActionResult Delete(int settingId)
-        {
-            _settingService.Delete(settingId);
-
-            return RedirectToAction(nameof(Index));
-        }
 
         [HttpGet]
         public ActionResult Setup()
