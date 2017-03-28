f6f35e635067a1bcdc7994b38dcabb449d95916c
Tom McClean <tommcclean@hotmail.co.uk>
2017-03-28 22:04:08 +0100

    Add PostExtension for BannerImagePath

---
+++ Portal.CMS.Web/Architecture/Extensions/PostExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Portal.CMS.Entities.Entities;
+using Portal.CMS.Entities.Enumerators;
+using System.Linq;
+
+namespace Portal.CMS.Web.Architecture.Extensions
+{
+    public static class PostExtensions
+    {
+        public const string DEFAULT_IMAGE_PATH = "/Areas/Builder/Content/Images/Sample/Sample-1.jpg";
+
+        public static string BannerImageUrl(this Post post)
+        {
+            if (post.PostImages.Any(x => x.PostImageType == PostImageType.Banner))
+                return post.PostImages.First(x => x.PostImageType == PostImageType.Banner).Image.ImagePath;
+
+            return DEFAULT_IMAGE_PATH;
+        }
+    }
+}
