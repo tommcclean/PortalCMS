9e2d46fc59b1a952dd2e43f56d8b7a9455bdd68e
Tom McClean <tommcclean@hotmail.co.uk>
2018-09-14 20:14:42 +0100

    Show the Menu Background Colour from the Current Theme in the Android Tab Menu and update to .NET 4.6.2

---
+++ Portal.CMS.Web/Architecture/Helpers/ThemeHelper.cs
@@ -0,0 +1,47 @@
+﻿using Portal.CMS.Services.Themes;
+using Portal.CMS.Web.DependencyResolution;
+
+namespace Portal.CMS.Web.Architecture.Helpers
+{
+    public static class ThemeHelper
+    {
+        public static string GetThemeColour()
+        {
+            var keyName = "ThemeColour";
+
+            var validSession = System.Web.HttpContext.Current.Session != null;
+
+            object sessionSetting = null;
+
+            if (validSession)
+            {
+                sessionSetting = System.Web.HttpContext.Current.Session[keyName];
+            }
+
+            if (sessionSetting != null)
+            {
+                return sessionSetting.ToString();
+            }
+            else
+            {
+                var container = IoC.Initialize();
+
+                var themeService = container.GetInstance<ThemeService>();
+
+                var theme = AsyncHelpers.RunSync(() => themeService.GetDefaultAsync());
+
+                if (theme == null)
+                {
+                    return "#FFFFFF";
+                }
+
+                if (validSession)
+                {
+                    System.Web.HttpContext.Current.Session.Add(keyName, theme.MenuBackgroundColour);
+                }
+
+                return theme.MenuBackgroundColour;
+            }
+        }
+    }
+}
