ec304fed45892786fdf49a5b4f022c8d2a1d0e34
SohailQureshi <sohail.qureshi@property-want.com>
2019-02-06 23:02:19 +0000

    Updated PortalDbContext with Identity Framework (Not fully implemented)

--- Portal.CMS.Entities/Entities/UserRole.cs
+++ Portal.CMS.Entities/Entities/UserRole.cs
@@ -3,19 +3,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Portal.CMS.Entities.Entities
 {
-    public class UserRole
-    {
-        [Key]
-        public int Id { get; set; }
+	public class UserRole
+	{
+		[ForeignKey("User")]
+		[Key, Column(Order = 1)]
+		public int UserId { get; set; }
 
-        [ForeignKey("User")]
-        public int UserId { get; set; }
+		[ForeignKey("Role")]
+		[Key, Column(Order = 2)]
+		public int RoleId { get; set; }
 
-        [ForeignKey("Role")]
-        public int RoleId { get; set; }
+		public virtual User User { get; set; }
 
-        public virtual User User { get; set; }
-
-        public virtual Role Role { get; set; }
-    }
+		public virtual Role Role { get; set; }
+	}
 }
--- Portal.CMS.Entities/Seed/RoleSeed.cs
+++ Portal.CMS.Entities/Seed/RoleSeed.cs
@@ -1,35 +1,56 @@
-﻿using Portal.CMS.Entities.Entities;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Portal.CMS.Entities.Entities;
+using Portal.CMS.Entities.Entities.Models;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Portal.CMS.Entities.Seed
 {
-    public static class RoleSeed
-    {
-        public static void Seed(PortalDbContext context)
-        {
-            var roleList = context.Roles.ToList();
+	public static class RoleSeed
+	{
+		public static void Seed(PortalDbContext context)
+		{
+			//var RoleManager = new CustomRoleManager(new RoleStore<IdentityRole>(context));
+			//var roleCount = RoleManager.Roles.Count();
 
-            var newRoles = new List<Role>();
+			//CreateRole(RoleManager, "Admin");
+			//CreateRole(RoleManager, "Editor");
+			//CreateRole(RoleManager, "Authenticated");
+			//CreateRole(RoleManager, "Anonymous");
 
-            if (!roleList.Any(x => x.Name == "Admin"))
-                newRoles.Add(new Role { Name = "Admin", IsAssignable = true });
+			var roleList = context.Roles.ToList();
 
-            if (!roleList.Any(x => x.Name == "Editor"))
-                newRoles.Add(new Role { Name = "Editor", IsAssignable = true });
+			var newRoles = new List<Role>();
 
-            if (!roleList.Any(x => x.Name == "Authenticated"))
-                newRoles.Add(new Role { Name = "Authenticated", IsAssignable = false });
+			if (!roleList.Any(x => x.Name == "Admin"))
+				newRoles.Add(new Role { Name = "Admin", IsAssignable = true });
 
-            if (!roleList.Any(x => x.Name == "Anonymous"))
-                newRoles.Add(new Role { Name = "Anonymous", IsAssignable = false });
+			if (!roleList.Any(x => x.Name == "Editor"))
+				newRoles.Add(new Role { Name = "Editor", IsAssignable = true });
 
-            if (newRoles.Any())
-            {
-                context.Roles.AddRange(newRoles);
+			if (!roleList.Any(x => x.Name == "Authenticated"))
+				newRoles.Add(new Role { Name = "Authenticated", IsAssignable = false });
 
-                context.SaveChanges();
-            }
-        }
-    }
+			if (!roleList.Any(x => x.Name == "Anonymous"))
+				newRoles.Add(new Role { Name = "Anonymous", IsAssignable = false });
+
+			if (newRoles.Any())
+			{
+				context.Roles.AddRange(newRoles);
+
+				context.SaveChanges();
+			}
+		}
+
+		private static void CreateRole(CustomRoleManager roleManager, string roleName)
+		{
+			var role = roleManager.FindByName(roleName);
+			if (role == null)
+			{
+				role = new IdentityRole(roleName);
+				roleManager.Create(role);
+			}
+		}
+	}
 }
