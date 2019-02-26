377286ddc052ae68da64aedce6ce73280c24c514
Suhail Qureshi <sohail.qureshi@property-want.com>
2019-02-26 23:33:42 +0000

    Admin page changes

--- PortalCMS.Web/Areas/Admin/ViewModels/UserManager/DetailsViewModel.cs
+++ PortalCMS.Web/Areas/Admin/ViewModels/UserManager/DetailsViewModel.cs
@@ -4,24 +4,25 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PortalCMS.Web.Areas.Admin.ViewModels.UserManager
 {
-    public class DetailsViewModel
-    {
-        public string UserId { get; set; }
+	public class DetailsViewModel
+	{
+		public string UserId { get; set; }
 
-        [DisplayName("Email")]
-        [Required]
-        public string EmailAddress { get; set; }
+		[Required]
+		[DisplayName("Email")]
+		public string EmailAddress { get; set; }
 
-        [DisplayName("First Name")]
-        [Required]
-        public string GivenName { get; set; }
+		[Required]
+		[DisplayName("First Name")]
+		public string GivenName { get; set; }
 
-        [DisplayName("Last Name")]
-        [Required]
-        public string FamilyName { get; set; }
+		[Required]
+		[DisplayName("Last Name")]
+		public string FamilyName { get; set; }
 
-        public DateTime DateAdded { get; set; }
+		[DisplayName("Registered")]
+		public DateTime DateAdded { get; set; }
 
-        public DateTime DateUpdated { get; set; }
-    }
+		public DateTime DateUpdated { get; set; }
+	}
 }
